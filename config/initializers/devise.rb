febf737f7b166362742ace5ce777cd417347a258
jaxgeller <jacksongeller@gmail.com>
2015-10-14 14:30:48 -0400

    cleanup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
   config.warden do |manager|
     manager.failure_app = AuthFail
   end
