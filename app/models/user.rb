3e7aa6cfd7132a3b9472968f513cd07aa7bc90ca
jaxgeller <jacksongeller@gmail.com>
2015-10-17 11:17:54 -0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ActiveRecord::Base
   has_secure_password
   before_create :generate_token
 
-  validates :email, uniqueness: true
-  validates :username, uniqueness: true
-  validates :token, uniqueness: true
+  validates :email, :username, :token, uniqueness: true
+  validates :email, :username, presence: true
 
   def generate_token
     self.token = SecureRandom.urlsafe_base64
