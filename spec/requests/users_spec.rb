91db7658b41a1236ac0eb2ef2c417ea7440fbea5
jaxgeller <jacksongeller@gmail.com>
2015-10-22 17:38:50 -0400

    pass failing tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -28,16 +28,18 @@ describe 'Users API' do
   end
 
   it 'should be able to delete a user' do
-    delete "/users/#{user.username}", {}, auth_headers(User.last)
+    user = User.last
+    delete "/users/#{user.username}", {}, auth_headers(user)
 
     expect(response.status).to eq 200
     expect(User.find_by_username(user.username)).to eql nil
   end
 
   it 'should be able to update password' do
+    user = User.last
     password = '123456password'
-    user = { password: password, password_confirmation: password }
-    put "/users/#{user.username}", { user: params }, auth_headers(User.last)
+    params = { password: password, password_confirmation: password }
+    put "/users/#{user.username}", { user: params }, auth_headers(user)
 
     body = JSON.parse(response.body)
 
