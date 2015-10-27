fd96921066d27c3b38d4025e2f7cce0545959c8b
jaxgeller <jacksongeller@gmail.com>
2015-10-27 11:35:06 -0400

    add better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,14 @@ User.create(username: 'jackson', email: 'jackson@gmail.com', password: 'password
 User.create(username: 'geller', email: 'geller@gmail.com', password: 'password')
 User.create(username: 'robert', email: 'robert@gmail.com', password: 'password')
 
-text = Faker::Lorem.paragraphs.join ' '
+startup_ipsum = File.open("db/ipsum_data/startup_ipsum.txt", "r").read
+Ipsum.create(title: 'Startup Ipsum', text: startup_ipsum, user: User.first, g_markov: false)
 
-Ipsum.create(title: 'Startup Ipsum', text: text, user: User.first)
+hipster_ipsum = File.open("db/ipsum_data/hipster_ipsum.txt", "r").read
+Ipsum.create(title: 'Hipster Ipsum', text: hipster_ipsum, user: User.all.sample, g_markov: false)
 
+bluth_ipsum = File.open("db/ipsum_data/bluth_ipsum.txt", "r").read
+Ipsum.create(title: 'Bluth Ipsum', text: bluth_ipsum, user: User.last)
 
-Ipsum.create(title: 'Super Hero Ipsum', text: text, user: User.all.sample, g_markov: false)
-
-
-Ipsum.create(title: 'VC Ipsum', text: text, user: User.all.sample)
-
-
-Ipsum.create(title: 'Batman Ipsum', text: text, user: User.all.sample, g_markov: false)
-
-
-Ipsum.create(title: 'Business Ipsum', text: text, user: User.all.sample)
+Ipsum.create(title: 'Lorem Ipsum', text: Faker::Lorem.paragraphs.join(' '), user: User.all.sample)
+Ipsum.create(title: 'Merol Ipsum', text: Faker::Lorem.paragraphs.join(' '), user: User.all.sample)
