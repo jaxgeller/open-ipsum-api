f79403501396386a0497a01e6314e88e2a2f0277
jaxgeller <jacksongeller@gmail.com>
2015-10-27 11:14:09 -0400

    add better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@ User.create(username: 'robert', email: 'robert@gmail.com', password: 'password')
 text = Faker::Lorem.paragraphs.join ' '
 
 Ipsum.create(title: 'Startup Ipsum', text: text, user: User.first)
+
+
 Ipsum.create(title: 'Super Hero Ipsum', text: text, user: User.all.sample, g_markov: false)
+
+
 Ipsum.create(title: 'VC Ipsum', text: text, user: User.all.sample)
+
+
 Ipsum.create(title: 'Batman Ipsum', text: text, user: User.all.sample, g_markov: false)
+
+
 Ipsum.create(title: 'Business Ipsum', text: text, user: User.all.sample)
