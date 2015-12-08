3c1aef53e64f3d1c30f77c5b88d2c0874f3bcb31
jaxgeller <jacksongeller@gmail.com>
2015-12-08 12:47:50 -0500

    add new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-User.create(username: 'jackson', email: 'jackson@gmail.com', password: 'password', password_confirmation: 'password')
-User.create(username: 'geller', email: 'geller@gmail.com', password: 'password', password_confirmation: 'password')
-User.create(username: 'robert', email: 'robert@gmail.com', password: 'password', password_confirmation: 'password')
+password = ENV['OPENIPSUM_USER_PASS'] || 'password'
+
+User.create(username: 'jackson', email: 'jackson@gmail.com', password: password, password_confirmation: password)
+User.create(username: 'jeff', email: 'geller@gmail.com', password: password, password_confirmation: password)
 
 startup_ipsum = File.open("db/ipsum_data/startup_ipsum.txt", "r").read
 Ipsum.create(title: 'Startup Ipsum', text: startup_ipsum, user: User.first, g_markov: false)
@@ -11,5 +12,12 @@ Ipsum.create(title: 'Hipster Ipsum', text: hipster_ipsum, user: User.all.sample,
 bluth_ipsum = File.open("db/ipsum_data/bluth_ipsum.txt", "r").read
 Ipsum.create(title: 'Bluth Ipsum', text: bluth_ipsum, user: User.last)
 
+pulp_ipsum = File.open("db/ipsum_data/pulp_ipsum.txt", "r").read
+Ipsum.create(title: 'Pulp Ipsum', text: pulp_ipsum, user: User.all.sample)
+
+
+
+
+
+
 Ipsum.create(title: 'Lorem Ipsum', text: Faker::Lorem.paragraphs.join(' '), user: User.all.sample)
-Ipsum.create(title: 'Merol Ipsum', text: Faker::Lorem.paragraphs.join(' '), user: User.all.sample)
